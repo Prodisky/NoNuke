49e227e28a11196e5d4faa373724bd5db982f0cc
Paul Wu <500@paul500.net>
2012-06-28 10:32:23 +0800

    Initial Commit

---
+++ NoNuke/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NoNuke
+//
+//  Created by Paul Wu on 12/6/28.
+//  Copyright (c) 2012年 Prodisky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
